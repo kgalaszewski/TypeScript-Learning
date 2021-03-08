dff40038d541e8dd9fb874cdd2cfa0fd716c9301
Karol Gałaszewski <kgalaszewski95@gmail.com>
2021-03-08 16:04:39 +0100

    added loops

---
+++ Loops.ts
@@ -0,0 +1,30 @@
+for (let i; i < 5; i++) {
+    // do stuff
+}
+
+let listaaa = [1, 2, 4, 5];
+
+for (let index in listaaa) {
+     // key is equal to index
+    let item = listaaa[index];
+}
+
+for (let item of listaaa) {
+    // here we already have items one by one
+}
+
+listaaa.forEach(item => {
+    // close to for of
+});
+
+// for await (let iterator of object) {
+    
+// }
+
+while (true) {
+    // do stuff
+}
+
+do {
+    // do stuff
+} while (true)
--- Tuples.js
+++
@@ -1,6 +0,0 @@
-var someTuple;
-someTuple = [2, 's'];
-var listOfTuples;
-listOfTuples.push([1, 'a'], [2, 'b']);
-listOfTuples.push([3, 'c']);
-console.log(listOfTuples);
